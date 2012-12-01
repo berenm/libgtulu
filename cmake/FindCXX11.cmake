b43838f456db86b13e07336abf34574b22ca7f99
Beren Minor <beren.minor+git@gmail.com>
2012-12-01 09:41:08 +0100

    BUILD: Add Travis-CI configuration file.

---
+++ cmake/FindCXX11.cmake
@@ -0,0 +1,12 @@
+include(CheckCXXCompilerFlag)
+enable_language(CXX)
+
+check_cxx_compiler_flag("-std=c++11" COMPILER_KNOWS_CXX11)
+if(COMPILER_KNOWS_CXX11)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+else()
+  check_cxx_compiler_flag("-std=c++0x" COMPILER_KNOWS_CXX0X)
+  if(COMPILER_KNOWS_CXX0X)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
+  endif()
+endif()
